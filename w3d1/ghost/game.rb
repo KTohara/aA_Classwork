492e13648e7a9f4a5acf84f0bab19dff8dc4dbe4
Kenta <ktohara@gmail.com>
2021-12-21 12:55:45 -0800

    Added multiplayer function

--- w3d1/ghost/game.rb
+++ w3d1/ghost/game.rb
@@ -2,39 +2,117 @@ require_relative 'player'
 require 'set'
 require 'byebug'
 
-
-# Instantiate a new Game object, passing in both of the Players.
-    # The Game maintains a fragment instance variable, which represents the word as it has been built up by the players.
-
-    # The Players take turns adding a letter to the fragment. The Game should ensure that a play is valid before actually changing the fragment.
-    
-    # The Game checks the fragment against a dictionary; if the fragment is a word contained in the dictionary, then the previous player loses.
-
-    
 class Game
 
     ABC = Set.new("a".."z")
-    attr_reader :players, :fragment, :dictionary, :losses
-    attr_writer :fragment
 
     def initialize(*players)
         @players = players
         @fragment = ""
         @losses = Hash.new { |losses, player| losses[player] = 0 }
+        players.each { |player| losses[player] = 0 }
         words = File.open("dictionary.txt").read.split
         @dictionary ||= Set.new(words)
     end
 
-    def current_player # array
+    # Helper Methods
+
+    def current_player
         players.first
     end
 
     def previous_player
-        players.last
+        players.reverse.each do |player|
+            return player if !player_lost?(player)
+        end
     end
 
     def next_player!
         players.rotate!
+        until !player_lost?(current_player)
+            players.rotate!
+        end
+    end
+
+    def player_lost?(player)
+        losses[player] >= 5
+    end
+
+    def valid_play?(string)
+        return false if !ABC.include?(string)
+        possible_word = @fragment + string
+        dictionary.any? { |word| word.start_with?(possible_word) }
+    end
+
+    def remaining_players
+        players.reject { |player| player_lost?(player) }
+    end 
+
+    def win_game?
+        remaining_players.count == 1
+    end
+
+    # UI Methods
+
+    def record(player)
+        ghost = "G H O S T"
+        games_lost = losses[player]
+        ghost[0...games_lost * 2]
+    end
+
+    def display_standings
+        puts "Current Standings: \n\n"
+        players.each do |player|
+            puts "#{player.name}: #{record(player)}"
+        end
+        sleep(1)
+    end
+
+    # Game Mechanic Methods
+
+    def run
+        system("clear")
+        puts "Ghost"
+        puts "Players must take turns putting a letter into the fragment"
+        puts "If a player finishes a word, they lose a round"
+        puts "When a player reaches 5 losses, the game is over"
+        puts "Press Enter to play"
+        enter = gets.chomp
+        if enter == ""
+            play_round until win_game?
+            puts "#{current_player.name} wins!"
+        else
+            run
+        end
+    end
+
+    def play_round
+        @fragment = ""
+        system("clear")
+        display_standings
+        sleep(2)
+        until lose_round?
+            take_turn(current_player)
+            next_player!
+        end
+    end
+
+    def lose_round?
+        if @dictionary.include?(@fragment)
+            puts "#{@fragment} is a word in the dictionary, #{previous_player.name} loses a round"
+            sleep(2)
+            player_eliminated?
+        end
+    end
+
+    def player_eliminated?
+        if losses[previous_player] == 4
+            puts "#{previous_player.name} has reached 'G H O S T'"
+            puts "#{previous_player.name} has been eliminated :("
+            sleep(2)
+        end
+        losses[previous_player] += 1
+
     end
 
     def take_turn(player)
@@ -53,74 +131,15 @@ class Game
         puts "#{current_player.name} put '#{guess}' into the fragment"
         sleep(1)
     end
-
-    def valid_play?(string)
-        return false if !ABC.include?(string)
-        possible_word = @fragment + string
-        dictionary.any? { |word| word.start_with?(possible_word) }
-    end
-
-    def record(player)
-        ghost = "GHOST"
-        games_lost = losses[player]
-        ghost[0...games_lost]
-    end
-
-    def play_round
-        @fragment = ""
-        sleep(1)
-        system("clear")
-        display_standings
-        until lose_round?
-            take_turn(current_player)
-            next_player!
-        end
-    end
-
-    def display_standings
-        puts "Current Standings: \n\n"
-        players.each do |player|
-            puts "#{player.name}: #{record(player)}"
-        end
-        sleep(1)
-    end 
-
-    def run
-        system("clear")
-        puts "Ghost"
-        puts "Players must take turns putting a letter into the fragment"
-        puts "If a player finishes a word, they lose a round"
-        puts "When a player reaches 5 losses, the game is over"
-        puts "Press Enter to play"
-        enter = gets.chomp
-        if enter == ""
-            play_round until lose_game?
-            puts "#{current_player.name} loses!"
-        else
-            run
-        end
-    end
-
-    def lose_game?
-        losses.values.any? { |val| val == 5 }
-    end
-
-    def lose_round?
-        if @dictionary.include?(@fragment)
-            puts "#{@fragment} is a word in the dictionary, #{previous_player.name} loses a round"
-            losses[previous_player] += 1
-        end
-    end
-
-
     private
-    
+    attr_reader :players, :fragment, :dictionary, :losses
 end
 
 if $PROGRAM_NAME == __FILE__
     game = Game.new(
     Player.new("George"),
-    Player.new("Nemo")
+    Player.new("Lenny"),
+    Player.new("Rabbit")
     )
     game.run
 end
